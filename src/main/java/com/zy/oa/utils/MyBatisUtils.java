f2d4104709e153b612a2e0aa07cabdbdc640651a
zy <1661996964@qq.com>
2022-10-23 12:09:07 +0800

    封装执行select查询sql类,开,查,无论是否执行成功都闭合

--- src/main/java/com/zy/oa/utils/MyBatisUtils.java
+++ src/main/java/com/zy/oa/utils/MyBatisUtils.java
@@ -2,11 +2,13 @@ package com.zy.oa.utils;
 
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.function.Function;
 
 public class MyBatisUtils {
     //利用static静态属于类,不属于对象,保证全局唯一
@@ -25,6 +27,20 @@ public class MyBatisUtils {
         }
     }
 
+    /**
+     * 执行select查询sql
+     * @param func 要执行查询语句的代码块
+     * @return 查询结果
+     */
+  public static Object executeQuery(Function<SqlSession,Object> func){
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+       try {
+           Object obj = func.apply(sqlSession);
+           return obj;
+       }finally {
+           sqlSession.close();
+       }
+  }
 }
 
 
