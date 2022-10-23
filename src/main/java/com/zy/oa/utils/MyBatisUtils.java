0beed50a942a4983984b9c0ba875cca0a7baeb3c
zy <1661996964@qq.com>
2022-10-23 13:02:18 +0800

    执行INSERT/UPDATE/DELETE写操作的封装工具类

--- src/main/java/com/zy/oa/utils/MyBatisUtils.java
+++ src/main/java/com/zy/oa/utils/MyBatisUtils.java
@@ -12,7 +12,8 @@ import java.util.function.Function;
 
 public class MyBatisUtils {
     //利用static静态属于类,不属于对象,保证全局唯一
-    private  static SqlSessionFactory sqlSessionFactory = null;
+    private static SqlSessionFactory sqlSessionFactory = null;
+
     //利用静态块在初始化时实例化sqlSessionFactory
     static {
         Reader reader = null;
@@ -29,18 +30,41 @@ public class MyBatisUtils {
 
     /**
      * 执行select查询sql
+     *
      * @param func 要执行查询语句的代码块
      * @return 查询结果
      */
-  public static Object executeQuery(Function<SqlSession,Object> func){
+    public static Object executeQuery(Function<SqlSession, Object> func) {
         SqlSession sqlSession = sqlSessionFactory.openSession();
-       try {
-           Object obj = func.apply(sqlSession);
-           return obj;
-       }finally {
-           sqlSession.close();
-       }
-  }
+        try {
+            Object obj = func.apply(sqlSession);
+            return obj;
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    /**
+     * 执行INSERT/UPDATE/DELETE写操作SQL
+     * @param func 要执行写操作语句的代码块
+     * @return 写操作执行后的返回结果
+     */
+    public static Object executeUpdate(Function<SqlSession, Object> func) {
+        //openSession传入false参数代表手动提交/回滚事务
+        SqlSession sqlSession = sqlSessionFactory.openSession(false);
+        try{
+            Object obj = func.apply(sqlSession);
+            sqlSession.commit();
+            return obj;
+        }catch (Exception e){
+            sqlSession.rollback();
+            throw e;
+        }
+        finally {
+            sqlSession.close();
+        }
+
+    }
 }
 
 
