906c5ff0999a6a1eb67304ff0775460d067fccbf
zy <1661996964@qq.com>
2022-10-24 08:53:27 +0800

    实例化UserMapper对象，创建登录异常类

---
+++ src/main/java/com/zy/oa/mapper/exception/LoginException.java
@@ -0,0 +1,7 @@
+package com.zy.oa.mapper.exception;
+
+public class LoginException extends RuntimeException{
+    public LoginException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/zy/oa/service/UserService.java
@@ -0,0 +1,7 @@
+package com.zy.oa.service;
+
+import com.zy.oa.mapper.UserMapper;
+
+public class UserService {
+    private UserMapper userMapper = new UserMapper();
+}
