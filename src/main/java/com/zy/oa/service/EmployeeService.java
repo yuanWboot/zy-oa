5551ef753b9114f2f69d9577856deebbcfe6248b
zy <1661996964@qq.com>
2022-10-25 23:28:14 +0800

    selectLeader查询领导

--- src/main/java/com/zy/oa/service/EmployeeService.java
+++ src/main/java/com/zy/oa/service/EmployeeService.java
@@ -5,6 +5,10 @@ import com.zy.oa.entity.Employee;
 import com.zy.oa.mapper.EmployeeMapper;
 import com.zy.oa.utils.MyBatisUtils;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class EmployeeService {
     public Employee selectById(Long employeeId){
         Employee employee = (Employee)MyBatisUtils.executeQuery(sqlSession -> {
@@ -17,12 +21,22 @@ public class EmployeeService {
         MyBatisUtils.executeQuery(sqlSession -> {
             EmployeeMapper employeeMapper = sqlSession.getMapper(EmployeeMapper.class);
             Employee employee = employeeMapper.selectById(employeeId);
+            Map params = new HashMap<>();
+            Employee leader = null;
             if (employee.getLevel()>7){
                 //查询部门经理
+                params.put("level",7);
+                params.put("department",employee.getDepartmentId());
+                List<Employee> employees = employeeMapper.selectByParams(params);
+                leader = employees.get(0);
             }else if(employee.getLevel() == 7){
                 //查询总经理
+                params.put("level",8);
+                List<Employee> employees = employeeMapper.selectByParams(params);
+                leader = employees.get(0);
             }else if (employee.getLevel() == 8){
                 //返回自己
+                leader = employee;
             }
             return null;
         });
