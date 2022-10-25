65d5ee820a95bd58efa9af1643a5d860be38a467
zy <1661996964@qq.com>
2022-10-25 19:51:21 +0800

    LeaveFormMapperTest测试用例

---
+++ src/test/java/com/zy/oa/mapper/LeaveFormMapperTest.java
@@ -0,0 +1,40 @@
+package com.zy.oa.mapper;
+
+import com.zy.oa.entity.LeaveForm;
+import com.zy.oa.utils.MyBatisUtils;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class LeaveFormMapperTest {
+
+    @Test
+    public void insert() {
+        MyBatisUtils.executeUpdate(sqlSession -> {
+          LeaveFormMapper mapper = sqlSession.getMapper(LeaveFormMapper.class);
+          LeaveForm form = new LeaveForm();
+          form.setEmployeeId(4l); //员工编号
+          form.setFormType(1); //事假
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date startTime = null; //开始时间
+            Date endTime = null; //结束时间
+            try {
+                startTime = sdf.parse("2021-10-01 08:02:00");
+                endTime = sdf.parse("2021-10-11 18:02:00");
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            form.setStartTime(startTime);
+            form.setEndTime(endTime);
+            form.setReason("回家收租");
+            form.setCreateTime(new Date());
+            form.setState("processing");
+            mapper.insert(form);
+            return null;
+        });
+    }
+}
